8b712cc71d657d7d2c4d4023a079583cff825f23
Irvin Contreras <irvincnt93@gmail.com>
2017-12-12 19:28:03 -0600

    iterar pruductos en el componente ProductList

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,47 @@ import { Container } from 'semantic-ui-react'
 import ProductList from '../ProductList'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      products: [
+        {
+          name: 'Camara Nikon D5500 24.2 Mp',
+          picture: 'https://http2.mlstatic.com/camara-D_Q_NP_100101-MLM20255371968_032015-X.webp',
+          price: '$ 12,346',
+          datails: 'Compra Protegida, recibe el producto que esperabas o te devolvemos tu dinero.',
+          marca: 'Nikon',
+          shipping: 'Envío gratis a todo el país',
+          status: '21 vendidos'
+
+        },
+        {
+          name: 'Camara Nikon D5500 24.2 Mp',
+          picture: 'https://http2.mlstatic.com/camara-D_Q_NP_100101-MLM20255371968_032015-X.webp',
+          price: '$ 12,346',
+          datails: 'Compra Protegida, recibe el producto que esperabas o te devolvemos tu dinero.',
+          marca: 'Nikon',
+          shipping: 'Envío gratis a todo el país',
+          status: '21 vendidos'
+        },
+        {
+          name: 'Camara Nikon D5500 24.2 Mp',
+          picture: 'https://http2.mlstatic.com/camara-D_Q_NP_100101-MLM20255371968_032015-X.webp',
+          price: '$ 12,346',
+          datails: 'Compra Protegida, recibe el producto que esperabas o te devolvemos tu dinero.',
+          marca: 'Nikon',
+          shipping: 'Envío gratis a todo el país',
+          status: '21 vendidos'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <Container>
         <h1>HOLA</h1>
-        <ProductList />
+        <ProductList products={this.state.products}/>
       </Container>
     )
   }
--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -2,11 +2,20 @@ import React, { Component }  from 'react'
 import Product from '../Product'
 
 class ProductList extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <div>
         ProductList
       <Product/>
+      {this.props.products.map( p => {
+        return (
+          <span>{p.name}</span>
+        )
+      })}
       </div>
 
     )
