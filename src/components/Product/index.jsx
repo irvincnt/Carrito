35cda86bccaf360c24d32d076a4afef0d0f23e9a
Irvin Contreras <irvincnt93@gmail.com>
2018-01-04 00:33:20 -0600

    limpiar codifo

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -8,18 +8,13 @@ class Product extends Component {
 
   numberFormat(amount, decimals) {
     decimals = decimals || 0;
-
     if (isNaN(amount) || amount === 0) return parseFloat(0).toFixed(decimals);
-
     amount = '' + amount.toFixed(decimals);
-
     var amount_parts = amount.split('.'), regexp = /(\d+)(\d{3})/;
-
     while (regexp.test(amount_parts[0]))
       amount_parts[0] = amount_parts[0].replace(regexp, '$1' + ',' + '$2');
-
     return amount_parts.join('.');
-}
+  }
 
   render() {
     return(
